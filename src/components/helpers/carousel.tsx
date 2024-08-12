fe761e54a9b42d0077e3a499bce504ac875ad93c
dchima <dcnnad@gmail.com>
2024-08-12 12:46:11 +0100

    fixes

--- src/components/helpers/carousel.tsx
+++ src/components/helpers/carousel.tsx
@@ -25,7 +25,6 @@ const carousel = () => {
                 <div className='container w-full mx-auto flex-col flex-wrap items-center justify-center text-center text-white md:w-3/4 md:mt-20 lg:w-2/4 lg:mt-15'>
                     <h6 className="text-xs text-[#fff] font-bold uppercase md:text-xl">Welcome to LPV Technologies</h6>
                     <h1 className=" text-4xl pt-5 font-bold md:text-6xl">Cleaner Energies for a Cleaner World</h1>
-                    <h4 className="text-lg pt-6 font-bold md:text-2xl">Our commitment to engineering excellence and craftsmanship drives us to create sustainable energy for a brighter future.</h4>
                     <div className="flex flex-col gap-y-5 justify-center items-center space-x-4 mt-10 md:flex md:flex-row md:gap-y-0">
                         <Button path="#" className="py-3 px-24 font-bold text-white bg-green rounded-full baseline hover:bg-darkBlue" text="Get a Quote" />
                         <Button path="#" className="flex justify-center items-center font-bold py-3 px-20 text-white bg-transparent border-solid border-2 border-white rounded-full baseline hover:border-darkBlue hover:bg-darkBlue" text="Discover More" />
