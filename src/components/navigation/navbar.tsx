905e04dd3e74544a5938504a86c4b54247f8e2ea
Esho Oluwasegun <eshooluwasegun98@gmail.com>
2024-08-27 10:07:58 +0100

    added navbar functionality

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { IoMdClose } from 'react-icons/io';
 import { RxHamburgerMenu } from 'react-icons/rx';
 import { MdKeyboardArrowDown } from 'react-icons/md';
@@ -12,17 +12,41 @@ const { navlist, subNav } = Data
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false)
     const [openMenu, setOpenMenu] = useState(null)
+    const [showNavbar, setShowNavbar] = useState(true)
+    const [lastScrollY, setLastScrollY] = useState(0)
+    const threshold = 200
 
     function handleToggle(type: string, name?: any){
         type === 'menu' ? setIsOpen(!isOpen) : setOpenMenu(openMenu === name ? null : name)
     }
 
+    const handleScroll = () => {
+        const currentScrollY = window.scrollY;
+
+        if (currentScrollY > lastScrollY && currentScrollY > threshold) {
+            setShowNavbar(false)
+        } else if (currentScrollY < lastScrollY || currentScrollY <= threshold) {
+            setShowNavbar(true)
+        }
+
+        setLastScrollY(currentScrollY);
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        // setShowNavbar(true);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+
+    }, [lastScrollY])
+
   return (
-        <nav className="_nav">
+        <nav className={`_nav ${showNavbar ? 'visible' : 'hidden'}`}>
             <div className="_container">
                 <div className="_flex_box">
                     <div className='_logo'>
-                        <a href='/'><img src={Logo} alt='logo' width={"140"} height={"140"} /></a>
+                        <a href='/'><img src={Logo} alt='logo' width={"100"} height={"100"} /></a>
                     </div>
                     <div className="_nav_menu">
                         <ul className="_nav_list">
@@ -40,14 +64,14 @@ const Navbar = () => {
                                         )
                                     }
                                     </li>
-                                    {subNav.includes(item.name) && <span><MdKeyboardArrowDown style={{ color: '#fff', paddingTop: '2px'}} size={'20px'} /></span>}
+                                    {subNav.includes(item.name) && <span><MdKeyboardArrowDown style={{ color:'#12263A', paddingTop: '2px'}} size={'20px'}  /></span>}
                                 </div>
                             ))}
                             {/* <div className='_nav_btn'>Get a Quote</div> */}
                         </ul>
                         <span className='_icon' onClick={() => handleToggle('menu')}>
                         {
-                            isOpen ? <IoMdClose size={'40px'} color='#fff' /> : <RxHamburgerMenu size={'40px'} color='#fff' />
+                            isOpen ? <IoMdClose size={'40px'} color='#12263A' /> : <RxHamburgerMenu size={'40px'} color='#12263A' />
                         }
                         </span>
                         {/** Mobile Version */}
@@ -79,7 +103,7 @@ const Navbar = () => {
                                             </motion.ul>
                                             )
                                         }
-                                        {subNav.includes(item.name) && <span className='icon_mobile'><MdKeyboardArrowDown size={'20px'} className='mt-[2px]' /></span>}
+                                        {subNav.includes(item.name) && <span className='icon_mobile'><MdKeyboardArrowDown size={'20px'} color='#12263A' className='mt-[2px]' /></span>}
                                         </motion.li>
                                     </div>
                                 ))}
